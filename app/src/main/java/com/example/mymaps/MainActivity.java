06236b81319d45776e5caa3c4cf7b75e34aca90f
YannuarSalim <yannuarsalim1@gmail.com>
2020-04-28 22:52:24 +0700

    commit 2

--- app/src/main/java/com/example/mymaps/MainActivity.java
+++ app/src/main/java/com/example/mymaps/MainActivity.java
@@ -14,20 +14,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    //    Fungsi untuk konek ke layer lain
     public void spbu(View view){
         String value="spbu";
         Intent intent = new Intent(MainActivity.this, MapsActivity.class);
-        //   Mengirim data untuk parameter di layer satunya
+
         intent.putExtra("key",value);
         startActivity(intent);
     }
 
-    //    Fungsi untuk konek ke layer lain
     public void universitas(View view){
         String value="universitas";
         Intent intent = new Intent(MainActivity.this, MapsActivity.class);
-        //   Mengirim data untuk parameter di layer satunya
         intent.putExtra("key",value);
         startActivity(intent);
     }
--- app/src/main/java/com/example/mymaps/MapsActivity.java
+++ app/src/main/java/com/example/mymaps/MapsActivity.java
@@ -24,30 +24,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        //   Mengambil data yang dikirim dari layer tadi
         Bundle extras = getIntent().getExtras();
         String value = extras.getString("key");
 
-
-
-        //   If else berdasar parameter yang dikirim.
         if (value.equalsIgnoreCase("spbu")){
 
             LatLng spbu1 = new LatLng(-7.983166, 112.614609);
@@ -67,7 +50,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             mMap.moveCamera(CameraUpdateFactory.newLatLng(spbu4));
 
             LatLng latLng = new LatLng(-7.985073, 112.626594);
-            float zoomLevel = 14.0f; //This goes up to 21
+            float zoomLevel = 14.0f;
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoomLevel));
 
         } else if (value.equalsIgnoreCase("universitas")){
@@ -77,18 +60,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             mMap.addMarker(new MarkerOptions().position(univ1).title("Universitas Brawijaya"));
             mMap.moveCamera(CameraUpdateFactory.newLatLng(univ1));
 
-            LatLng pom2 = new LatLng(-7.972467, 112.609535);
-            mMap.addMarker(new MarkerOptions().position(pom2).title("Universitas Merdeka"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(pom2));
+            LatLng univ2 = new LatLng(-7.972467, 112.609535);
+            mMap.addMarker(new MarkerOptions().position(univ2).title("Universitas Merdeka"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(univ2));
 
-            LatLng pom3 = new LatLng(-7.961273, 112.617393);
-            mMap.addMarker(new MarkerOptions().position(pom3).title("Universitas Negri Malang"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(pom3));
+            LatLng univ3 = new LatLng(-7.961273, 112.617393);
+            mMap.addMarker(new MarkerOptions().position(univ3).title("Universitas Negri Malang"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(univ3));
 
 
-            //   Objek ini khusus untuk lokasi user dan zoom levelnya
             LatLng latLng = new LatLng(-7.961273, 112.617393);
-            float zoomLevel = 14.0f; //This goes up to 21
+            float zoomLevel = 14.0f;
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoomLevel));
 
         }
